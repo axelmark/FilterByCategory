758792dd74a70abc6746ca1ff167f2174d07dd5d
axelmark <axelmark@outlook.com>
2017-03-03 15:58:48 +0700

    1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using DropDownList.Models;
+
+namespace DropDownList.Controllers {
+    public class HomeController : Controller
+    {
+        private fbEntities1 db = new fbEntities1();
+
+        public ActionResult Index(int? selId)
+        {
+            ViewData["idSel"] = selId;
+            ViewBag.list = new List<DropList>
+            {
+                new DropList {IDs = 1, Value = "Comedy"},
+                new DropList {IDs = 2, Value = "Family"},
+                new DropList {IDs = 3, Value = "Romance"},
+                new DropList {IDs = 4, Value = "Fantasy"}
+            };
+            var films = db.Films.Include(f => f.Category).Where(p=> p.cat_film==selId);
+            return View(films.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult index()
+        {
+            return View();
+        }
+    }
+}
